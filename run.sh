cfb9b8e4ea6dd2c3ba97b0c1909080a92f178904
Shreyas Sathe <47695170+SerraTorr@users.noreply.github.com>
2021-02-10 01:08:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-cp -f schedule.c /usr/src/Minix3/minix/servers/sched/schedule.c
-make build MKUPDATE=yes >log.txt 2>log.txt
+cd /usr/src
+cp -f schedule.c /usr/src/minix/servers/sched/schedule.c
+make build MKUPDATE=yes >log.txt 2>log2.txt
